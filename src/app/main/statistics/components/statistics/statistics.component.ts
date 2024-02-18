6c12fa3bc8122452a794c3da02b2acaae4cd6673
zubaerh <zubbaerhossain@gmail.com>
2024-02-18 11:24:55 +0600

    add statistics page

---
+++ src/app/main/statistics/components/statistics/statistics.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-statistics',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './statistics.component.html',
+    styleUrl: './statistics.component.scss',
+})
+export class StatisticsComponent {}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,12 +3,20 @@ import { Component } from '@angular/core'
 import { BannerComponent } from '@main/banner/components/banner/banner.component'
 import { NavbarComponent } from '@main/navbar/components/navbar/navbar.component'
 import { RegistrationComponent } from '@main/registration/components/registration/registration.component'
+import { StatisticsComponent } from '@main/statistics/components/statistics/statistics.component'
 import { DUIButton } from 'david-ui-angular'
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [CommonModule, DUIButton, NavbarComponent, BannerComponent, RegistrationComponent],
+    imports: [
+        CommonModule,
+        DUIButton,
+        NavbarComponent,
+        BannerComponent,
+        RegistrationComponent,
+        StatisticsComponent,
+    ],
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
 })

cb73a1d2d53247fb33e1eaff3359271512854f87
zubaerh <zubbaerhossain@gmail.com>
2024-02-22 05:19:50 +0600

    add nav bar dropdown

--- src/app/main/navbar/components/navbar/navbar.component.ts
+++ src/app/main/navbar/components/navbar/navbar.component.ts
@@ -1,11 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatMenuModule } from '@angular/material/menu'
+import { RouterLink } from '@angular/router'
+import { getHomeRoutes } from '@pages/home/home.routes'
 
 @Component({
     selector: 'app-navbar',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, RouterLink, MatIconModule, MatButtonModule, MatMenuModule],
     templateUrl: './navbar.component.html',
     styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+    readonly homeRoutes = getHomeRoutes()
+}
--- src/app/pages/home/home.routes.ts
+++ src/app/pages/home/home.routes.ts
@@ -5,6 +5,7 @@ import { setLayout } from '@main/layout/set-layout.resolver'
 export type HomeRoutes = {
     index: Route
     redirect: Route
+    contractUs: Route
 }
 
 export function getHomeRoutes(): HomeRoutes {
@@ -19,6 +20,15 @@ export function getHomeRoutes(): HomeRoutes {
             path: 'home',
             redirectTo: '',
             pathMatch: 'full',
+        },
+        contractUs: {
+            path: 'home/contractUs',
+            title: 'ContractUs',
+            resolve: { layout: setLayout(PageLayout.Sidebar) },
+            loadComponent: () =>
+                import('@main/registration/components/registration/registration.component').then(
+                    (m) => m.RegistrationComponent,
+                ),
         },
     }
 }

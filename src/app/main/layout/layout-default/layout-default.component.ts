ad61a4304142f01f4a54afc4bdcd7dc4104f448f
Mushfiqur Rahman Akhand <37366996+shohan-akhand@users.noreply.github.com>
2024-03-07 03:24:58 +0600

    reconstructing the mattrika

--- src/app/main/layout/layout-default/layout-default.component.ts
+++ src/app/main/layout/layout-default/layout-default.component.ts
@@ -2,13 +2,14 @@ import { CommonModule } from '@angular/common'
 import { ChangeDetectionStrategy, Component } from '@angular/core'
 import { RouterModule } from '@angular/router'
 import { HeaderOneComponent } from '@main/headers/header-one/header-one.component'
+import { NavbarComponent } from "../../navbar/components/navbar/navbar.component";
 
 @Component({
     selector: 'app-layout-default',
     standalone: true,
-    imports: [CommonModule, RouterModule, HeaderOneComponent],
     templateUrl: './layout-default.component.html',
     styleUrls: ['./layout-default.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [CommonModule, RouterModule, HeaderOneComponent, NavbarComponent]
 })
 export class LayoutDefaultComponent {}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,57 @@ module.exports = {
     darkMode: 'media', // or 'class'
     theme: {
         extend: {
+
+            keyframes: {
+
+                 typing: {
+          "0%": {
+            width: "0%",
+            visibility: "hidden"
+          },
+          "100%": {
+            width: "100%"
+          }
+        },
+        blink: {
+          "50%": {
+            borderColor: "transparent"
+          },
+          "100%": {
+            borderColor: "white"
+          }
+        },
+
+
+         typewriter: {
+          to: {
+            left: '100%',
+          },
+        },
+        blink: {
+          '0%': {
+            opacity: '0',
+          },
+          '0.1%': {
+            opacity: '1',
+          },
+          '50%': {
+            opacity: '1',
+          },
+          '50.1%': {
+            opacity: '0',
+          },
+          '100%': {
+            opacity: '0',
+          },
+        },
+      },
+      animation: {
+        typewriter: 'typewriter 2s steps(11) forwards',
+        caret: 'typewriter 2s steps(11) forwards, blink 1s steps(11) infinite 2s',
+        typing: "typing 2s steps(20) infinite alternate, blink .7s infinite",
+      },
+
             colors: {
                 base: 'var(--base)',
                 'base-muted': 'var(--base-muted)',
@@ -25,6 +76,7 @@ module.exports = {
     },
     plugins: [
         require('@tailwindcss/aspect-ratio'),
+        require('daisyui'),
         require('@tailwindcss/forms'),
         require('@tailwindcss/typography'),
         require('tailwindcss-elevation'),

87b91577739cc931693c337a37b2d4299eca43e7
Touhid Rahman <touhidrahman1987@gmail.com>
2024-03-10 00:33:59 -0500

    Edit contents

--- src/app/main/team/components/our-team-one/our-team-one.component.ts
+++ src/app/main/team/components/our-team-one/our-team-one.component.ts
@@ -1,13 +1,39 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'
+import { Component } from '@angular/core'
 
 @Component({
     selector: 'app-our-team-one',
     standalone: true,
-    imports: [
-        CommonModule,
-    ],
+    imports: [CommonModule],
     templateUrl: './our-team-one.component.html',
     styleUrl: './our-team-one.component.scss',
 })
-export class OurTeamOneComponent { }
+export class OurTeamOneComponent {
+    items = [
+        {
+            name: 'A. Akhand',
+            position: 'Co-founder & CEO',
+            image: 'user.jpg',
+        },
+        {
+            name: 'T. Rahman',
+            position: 'Co-founder & CTO',
+            image: 'user.jpg',
+        },
+        {
+            name: 'Akramul Islam',
+            position: 'Co-founder & Manager (HR)',
+            image: 'akram.png',
+        },
+        {
+            name: 'Ariful Haque',
+            position: 'Web Developer',
+            image: 'arif.png',
+        },
+        {
+            name: 'Shohan Akhand',
+            position: 'Web Developer',
+            image: 'mushfiq.png',
+        },
+    ]
+}

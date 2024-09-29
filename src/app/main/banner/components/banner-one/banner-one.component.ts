537f57d269666a7573f79013547ff2d2c2b2ae73
ferdousr3 <ferdousr1992@gmail.com>
2024-09-29 11:59:01 +0600

    added new new animation in banner

--- src/app/main/banner/components/banner-one/banner-one.component.ts
+++ src/app/main/banner/components/banner-one/banner-one.component.ts
@@ -3,12 +3,19 @@ import { Component } from '@angular/core'
 import { RouterModule } from '@angular/router'
 import { NgxGradientTextComponent } from '@omnedia/ngx-gradient-text'
 import { NgxBackgroundBeamsComponent } from '@omnedia/ngx-background-beams'
+ import { NgxThreeGlobeComponent } from '@omnedia/ngx-three-globe'
 
-@Component({
-    selector: 'app-banner-one',
-    standalone: true,
-    imports: [CommonModule, RouterModule, NgxGradientTextComponent, NgxBackgroundBeamsComponent],
-    templateUrl: './banner-one.component.html',
-    styleUrl: './banner-one.component.scss',
-})
-export class BannerOneComponent {}
+ @Component({
+     selector: 'app-banner-one',
+     standalone: true,
+     imports: [
+         CommonModule,
+         RouterModule,
+         NgxGradientTextComponent,
+         NgxBackgroundBeamsComponent,
+         NgxThreeGlobeComponent,
+     ],
+     templateUrl: './banner-one.component.html',
+     styleUrl: './banner-one.component.scss',
+ })
+ export class BannerOneComponent {}

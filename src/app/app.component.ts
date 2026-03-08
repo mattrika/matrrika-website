21d2d1a04fa31f2df6a139447e18d9187d68198c
istiuak <istiuak.0@gmail.com>
2026-03-08 12:10:55 +0600

    clean up old angular project

--- src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core'
-import { RouterOutlet } from '@angular/router'
-import { LayoutTwoComponent } from '@main/layout/layout-two/layout-two.component'
-@Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    imports: [LayoutTwoComponent,RouterOutlet],
-})
-export class AppComponent {}
